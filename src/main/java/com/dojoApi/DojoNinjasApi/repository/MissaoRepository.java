966d2195e8c58891e2bf2af15784d151ccb639ed
robertperquim <perquimrobert@gmail.com>
2023-11-16 08:56:56 -0300

    Feat: criando missaoRepository

---
+++ src/main/java/com/dojoApi/DojoNinjasApi/repository/MissaoRepository.java
@@ -0,0 +1,7 @@
+package com.dojoApi.DojoNinjasApi.repository;
+
+import com.dojoApi.DojoNinjasApi.model.Missao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<Missao, Long> {
+}
