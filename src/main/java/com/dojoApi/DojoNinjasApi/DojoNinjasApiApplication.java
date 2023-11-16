72f2d5212f38ce0284017028302e1c4f1b5e4128
robertperquim <perquimrobert@gmail.com>
2023-11-16 07:12:33 -0300

    Feat: Criando projeto

---
+++ src/main/java/com/dojoApi/DojoNinjasApi/DojoNinjasApiApplication.java
@@ -0,0 +1,13 @@
+package com.dojoApi.DojoNinjasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoNinjasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoNinjasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojoApi/DojoNinjasApi/DojoNinjasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojoApi.DojoNinjasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoNinjasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
