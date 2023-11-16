b958b502ea6ca9f4c288ee029afd56b532be60e4
robertperquim <perquimrobert@gmail.com>
2023-11-16 07:54:30 -0300

    Feat: Ninjas controller

---
+++ src/main/java/com/dojoApi/DojoNinjasApi/controller/NinjaController.java
@@ -0,0 +1,26 @@
+package com.dojoApi.DojoNinjasApi.controller;
+
+import com.dojoApi.DojoNinjasApi.model.Ninja;
+import com.dojoApi.DojoNinjasApi.recordsClass.NinjaRegisterDate;
+import com.dojoApi.DojoNinjasApi.repository.NinjaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ninja")
+public class NinjaController {
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
+    @PostMapping("create")
+    @Transactional
+    public void createNinja (@RequestBody NinjaRegisterDate data){
+        ninjaRepository.save( new Ninja(data));
+    }
+
+
+}
