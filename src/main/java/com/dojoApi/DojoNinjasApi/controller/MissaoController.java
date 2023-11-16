d53b17f534307a1e4a86d9256cbce1920e27b00c
robertperquim <perquimrobert@gmail.com>
2023-11-16 08:55:48 -0300

    Feat: Criando controller missao

---
+++ src/main/java/com/dojoApi/DojoNinjasApi/controller/MissaoController.java
@@ -0,0 +1,24 @@
+package com.dojoApi.DojoNinjasApi.controller;
+
+import com.dojoApi.DojoNinjasApi.model.Missao;
+import com.dojoApi.DojoNinjasApi.recordsClass.MissaoRegisterDate;
+import com.dojoApi.DojoNinjasApi.repository.MissaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("missao")
+public class MissaoController {
+    @Autowired
+    private MissaoRepository missaoRepository;
+
+    @PostMapping("create")
+    @Transactional
+    public void createMissao (@RequestBody MissaoRegisterDate data){
+        missaoRepository.save( new Missao(data));
+    }
+}
